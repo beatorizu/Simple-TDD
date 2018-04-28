323a889007054c0e6ad02cf8746626a229a73cf4
beatorizu <betrizaf@gmail.com>
2018-04-28 15:41:06 -0300

    Adicionada a função de divisão
    Verifica se a função "retorna" uma exceção.
    assertRaises:
    https://stackoverflow.com/questions/129507/how-do-you-test-that-a-python-function-throws-an-exception/14712903#14712903

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,3 +6,8 @@ class Calculator(object):
     def sub(self, valueA: float, valueB: float) -> float:
         return valueA - valueB
 
+    def div(self, valueA: float, valueB: float) -> float:
+        try:
+            return valueA / valueB
+        except ZeroDivisionError:
+            raise ZeroDivisionError
--- calculator/tests/test_calculadora.py
+++ calculator/tests/test_calculadora.py
@@ -25,3 +25,19 @@ class SubNumTest(TestCase):
         sub_ = self.calc.sub(self.valueA, self.valueB)
         self.assertEqual(-1, sub_)
 
+
+class DivNumTest(TestCase):
+
+    def setUp(self):
+        self.valueA = 6
+        self.valueB = 2
+        self.valueC = 0
+        self.calc = Calculator()
+
+    def test_can_div_numbers(self):
+        div_ = self.calc.div(self.valueA, self.valueB)
+        self.assertEqual(3, div_)
+
+    def test_can_div_number_by_zero(self):
+        self.assertRaises(ZeroDivisionError,
+                          lambda: self.calc.div(self.valueA, self.valueC))
